2fb5ac9c8b6602bf6b8c99ad7950919e338fbf78
darrencompton <daz@coolbyte.co.uk>
2021-02-26 17:58:44 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import data from "./quotes.json";
+class App extends Component {
+  state = { index: 1 };
+  componentDidMount() {
+    this.setState({ index: this.getRandomIndex() });
+  }
+  getRandomIndex() {
+    return Math.floor(Math.floor(Math.random() * data.length));
+  }
+
+  render() {
+    const link =
+      "https://twitter.com/intent/tweet?text=" +
+      encodeURIComponent(data[this.state.index].Quote) +
+      "&via=" +
+      encodeURIComponent(data[this.state.index].Author);
+    console.log(link);
+    return (
+      <div id="wrapper">
+        <div id="quotebox">
+          <div id="text">{data[this.state.index].Quote}</div>
+          <div id="author">{data[this.state.index].Author}</div>
+          <div id="actions">
+            <button
+              style={{ marginLeft: "30px" }}
+              onClick={() => this.setState({ index: this.getRandomIndex() })}
+            >
+              New Quote
+            </button>
+
+            <a href={link} target="_blank" rel="noreferrer">
+              <button style={{ margin: "30px" }}>Tweet this</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
